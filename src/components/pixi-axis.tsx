89ffe6c80ff9a89b2fcf949a944f9f4a77c4550d
Noah Paessel <knowuh@gmail.com>
2019-04-11 14:35:57 -0400

    Center axis labels.

--- src/components/pixi-axis.tsx
+++ src/components/pixi-axis.tsx
@@ -9,12 +9,18 @@ interface IProps {
   gridMetrics: ICanvasShape;
 }
 
-const GridLabel = (props: {x: number, y: number, title: string}) => {
-  const style = new TextStyle({fill: "black", fontSize: "12px"});
-  const { x, y, title } = props;
+const GridLabel = (props: {x: number, y: number, title: string, top: boolean}) => {
+  const style = new TextStyle({fill: "black", fontSize: "12px", align: "center"});
+  const { x, y, title, top } = props;
+
   return (
     <Container position={[x, y]} >
-      <Text style={style} text={title}/>
+      {
+        top
+        ? <Text anchor={[0.5, 0]} style={style} text={title}/>
+        : <Text anchor={[0.0, 0.5]} style={style} text={title}/>
+      }
+
     </Container>
   );
 };
@@ -27,11 +33,11 @@ export const PixiAxis = (props: IProps) => {
   let x = 0;
   let y = 0;
   for (x = 0; x < numCols; x++) {
-    labels.push(<GridLabel x={x * gridSize + offset} y={0} title={`${x}`} />);
+    labels.push(<GridLabel top={true} x={x * gridSize + offset} y={0} title={`${x}`} />);
   }
   x = 0;
   for (y = 0; y < numRows; y++) {
-    labels.push(<GridLabel x={0} y={y * gridSize + offset} title={`${y}`}/>);
+    labels.push(<GridLabel top={false} x={0} y={y * gridSize + offset} title={`${y}`}/>);
   }
 
   return (
