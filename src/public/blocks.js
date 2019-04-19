a5ab94fbbc8c968815b7bc14c3690eda6746465e
Sam Fentress <sfentress@concord.org>
2019-04-19 13:46:37 -0400

    Add grid-coloring block

--- src/public/blocks.js
+++ src/public/blocks.js
@@ -401,6 +401,33 @@ Blockly.JavaScript['erupt'] = function(block) {
   return code;
 };
 
+/************************** results: ***********************************/
+
+Blockly.Blocks['outputPaintGrid'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Color the grid")
+        .appendField(new Blockly.FieldColour("#ff0000"), "COLOR")
+        .appendField("by")
+        .appendField(new Blockly.FieldDropdown([["tephra thickness", "thickness"]]), "result_type");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Color the results of the simulation in the grid with a color");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['outputPaintGrid'] = function(block) {
+  var resultType = block.getFieldValue('result_type');
+  var color = block.getFieldValue('COLOR');
+  // TODO: Assemble JavaScript into code variable.
+  var code = `
+    paintGrid({resultType: "${resultType}", color: "${color}"});
+  `;
+  return code;
+};
+
 /************************** reDraw: ***********************************/
 
 Blockly.Blocks['redrawMap'] = {
