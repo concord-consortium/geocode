643a67e5e96ff8fcd203341ed5ac228dffbd95c5
Noah Paessel <knowuh@gmail.com>
2019-04-24 14:14:33 -0400

    Remove some block assertions.
    We probably want to remove all assertions about the starter blocks until it can be authored.

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -8,15 +8,9 @@ context("Test app workspace", () => {
       cy.contains(".app",  "Blocks");
       cy.contains(".app",  "Code");
       // cy.get(".app").should("have.text", "Blocks");
-      // cy.get(".app").should("have.text", "Blocks");
     });
     it("Contains some town names in the blockly region", () => {
       cy.contains(".injectionDiv", "Concord");
-      cy.contains(".injectionDiv", "Amherst");
-      cy.contains(".injectionDiv", "Boston");
-    });
-    it("Contains a counting loop", () => {
-      cy.contains(".injectionDiv", "count");
     });
     it("cotains a command to set wind speed", () => {
       cy.contains(".injectionDiv", "wind");
