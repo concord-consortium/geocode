b0c2186582c4ec333be2a4b88e9074148c094b94
Saul Amster <saulamster@gmail.com>
2019-06-20 13:45:19 -0400

    visual improvements

--- src/components/pixi-cross-section-selector.tsx
+++ src/components/pixi-cross-section-selector.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { PixiComponent, Container } from "@inlet/react-pixi";
+import { PixiComponent, Container, Text } from "@inlet/react-pixi";
 import * as PIXI from "pixi.js";
+import { TextStyle } from "pixi.js";
+
 
 interface IProps {
     crossPoint1X: number;
@@ -18,29 +20,33 @@ export const CrossSection = PixiComponent<IProps, PIXI.Graphics>("CrossSection",
     create: (props) => new PIXI.Graphics(),
     applyProps: (g, _, props: IProps) => {
         const { crossPoint2X, crossPoint2Y, crossPoint1X, crossPoint1Y, isPlaced } = props;
-        let lineColor = 0x624d6b;
+        let lineColor = 0x7f7167;
 
         g.clear();
         if (!isPlaced) {
-            lineColor = 0xd0aae0;
+            lineColor = 0x686e77;
         }
-        g.lineStyle(3, lineColor, 1);
+        g.lineStyle(5, lineColor, 1);
         g.moveTo(crossPoint2X, crossPoint2Y);
         g.beginFill(lineColor, 1);
-        g.drawCircle(crossPoint2X, crossPoint2Y, 5);
+        g.drawCircle(crossPoint2X, crossPoint2Y, 11);
         g.endFill();
         g.moveTo(crossPoint2X, crossPoint2Y);
         g.lineTo(crossPoint1X, crossPoint1Y);
         g.beginFill(lineColor, 1);
-        g.drawCircle(crossPoint1X, crossPoint1Y, 5);
+        g.drawCircle(crossPoint1X, crossPoint1Y, 11);
         g.endFill();
     }
 });
 
 export class CrossSectionSelectorComponent extends React.Component<IProps, IState> {
 
+
     public render() {
         const { crossPoint2X, crossPoint2Y, crossPoint1X, crossPoint1Y, isPlaced } = this.props;
+
+        const style = new TextStyle({fill: "white", fontSize: `12px`, align: "center"});
+
         return (
             <Container>
                 <CrossSection
@@ -49,6 +55,20 @@ export class CrossSectionSelectorComponent extends React.Component<IProps, IStat
                     crossPoint1X={crossPoint1X}
                     crossPoint1Y={crossPoint1Y}
                     isPlaced={isPlaced} />
+                <Text
+                    anchor={[0.5, 0.5]}
+                    text={"P1"}
+                    x={crossPoint1X}
+                    y={crossPoint1Y}
+                    style={style}
+                />
+                <Text
+                    anchor={[0.5, 0.5]}
+                    text={"P2"}
+                    x={crossPoint2X}
+                    y={crossPoint2Y}
+                    style={style}
+                />
             </Container>
         );
     }
