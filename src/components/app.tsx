ccf919c12b7d62947637e79ea7622c301c0fc073
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-11-12 09:27:52 -0800

    Add widget to right panel, fix app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -541,7 +541,6 @@ export class AppComponent extends BaseComponent<IProps, IState> {
                   showCrossSection={true}
                   hasErupted={ hasErupted }
                 />
-<<<<<<< HEAD
                 <CrossSectionComponent
                   isSelectingCrossSection={isSelectingCrossSection}
                   showCrossSectionSelector={isSelectingCrossSection}
@@ -585,82 +584,6 @@ export class AppComponent extends BaseComponent<IProps, IState> {
                     label={{ value: plotData.yAxis, angle: -90, offset: 12, position: "insideBottomLeft" }}
                   />
                 </LineChart>
-=======
-              </LineChart>
-            }
-            { showSidebar &&
-              <MapSidebarComponent
-                width={ mapWidth - 300 }
-                height={ 100 }
-                windSpeed={ windSpeed }
-                windDirection={ windDirection }
-                colHeight={ colHeight }
-                vei={ vei }
-                mass={ mass }
-                particleSize={ particleSize }
-              />
-            }
-            { showOptionsDialog &&
-              <DatGui data={simulationOptions} onUpdate={this.handleUpdate}>
-              <DatButton label="Model options" onClick={this.toggleShowOptions} />
-              { expandOptionsDialog &&
-                [
-                  <DatBoolean path="requireEruption" label="Require eruption?" key="requireEruption" />,
-                  <DatBoolean path="requirePainting" label="Require painting?" key="requirePainting" />,
-                  <DatSelect path="scenario" label="Map Scenario" options={Object.keys(Scenarios)} key="background" />,
-                  <DatSelect path="toolbox" label="Code toolbox"
-                    options={Object.keys(BlocklyAuthoring.toolbox)} key="toolbox" />,
-                  <DatSelect path="initialCodeTitle" label="Initial code"
-                    options={Object.keys(BlocklyAuthoring.code)} key="code" />,
-
-                  <DatButton label="Save current code to local storage"
-                    onClick={this.saveCodeToLocalStorage}
-                    key="generate" />,
-                  <DatButton label="Load code from local storage"
-                    onClick={this.loadCodeFromLocalStorage}
-                    key="generate" />,
-
-                  <DatBoolean path="showCrossSection" label="Show cross section?" key="showCrossSection" />,
-                  <DatBoolean path="showChart" label="Show chart?"
-                    key="showChart" />,
-
-                  <DatBoolean path="showBlocks" label="Show blocks?" key="showBlocks" />,
-                  <DatBoolean path="showCode" label="Show code?" key="showCode" />,
-                  <DatBoolean path="showControls" label="Show controls?" key="showControls" />,
-                  <DatFolder title="Controls Options" key="controlsFolder" closed={true}>
-                    <DatBoolean path="showWindSpeed" label="Show Wind Speed?" key="showWindSpeed"/>
-                    <DatNumber
-                      path="initialWindSpeed" label="Initial Wind Speed" key="initialWindSpeed"
-                      min={0} max={30} step={1}/>
-                    <DatBoolean path="showWindDirection" label="Show Wind Direction?" key="showWindDirection" />
-                    <DatNumber
-                      path="initialWindDirection" label="Initial Wind Direction" key="initialWindDirection"
-                      min={0} max={360} step={1}/>
-                    <DatBoolean path="showEjectedVolume" label="Show Ejected Volume?" key="showEjectedVolume" />
-                    <DatNumber
-                      path="initialEruptionMass" label="Initial Ejection Volume" key="initialEruptionMass"
-                      min={100000000} max={10000000000000000} step={1000}/>
-                    <DatBoolean path="showColumnHeight" label="Show Column Height?" key="showColumnHeight" />
-                    <DatNumber
-                      path="initialColumnHeight" label="Initial Column Height" key="initialColumnHeight"
-                      min={1000} max={30000} step={1000}/>
-                    <DatBoolean path="showVEI" label="Show VEI?" key="showVEI" />
-                    <DatNumber
-                      path="initialVEI" label="Initial VEI" key="initialVEI"
-                      min={1} max={8} step={1}/>
-                  </DatFolder>,
-
-                  <DatBoolean path="showLog" label="Show Log?" key="showLog" />,
-
-                  <DatBoolean path="showChart" label="Show chart?" key="showChart" />,
-                  <DatBoolean path="showSidebar" label="Show sidebar?" key="showSidebar" />,
-                  // submit button. Should remain at bottom
-                  <DatButton
-                    label="Generate authored model"
-                    onClick={this.generateAndOpenAuthoredUrl}
-                    key="generate" />
-                ]
->>>>>>> Add column height widget
               }
               </div>
             </TabPanel>
