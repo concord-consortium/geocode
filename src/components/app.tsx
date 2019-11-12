c4f43dedc63a91a9f07ef26723db0385e569e379
mklewandowski <matt.kain.lewandowski@gmail.com>
2019-11-12 13:26:13 -0800

    Remove forceRender from Block TabPanel component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -271,7 +271,6 @@ export class AppComponent extends BaseComponent<IProps, IState> {
             { showBlocks &&
               <TabPanel
                 width={`${tabWidth}px`}
-                forceRender={true}
                 tabcolor={this.getTabColor(SectionTypes.BLOCKS)}
               >
                 <TabContent>
