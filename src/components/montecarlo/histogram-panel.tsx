e1d3403c95624045293023cf97c7e07096572002
mklewandowski <matt.kain.lewandowski@gmail.com>
2020-03-02 13:35:20 -0800

    Add risk level text and percent complete
    [#170909187]

--- src/components/montecarlo/histogram-panel.tsx
+++ src/components/montecarlo/histogram-panel.tsx
@@ -7,9 +7,9 @@ import { SvgD3HistogramChart } from "../charts/svg-d3-histogram-chart";
 import { ChartType } from "../../stores/charts-store";
 
 // TODO threshold and histogram min/max will need to be set elsewhere
-const kTephaThreshold = 201;
-const kTephaMin = 0;
-const kTephaMax = 400;
+const kTephraThreshold = 201;
+const kTephraMin = 0;
+const kTephraMax = 400;
 
 interface PanelProps {
   height: number;
@@ -23,9 +23,35 @@ const Panel = styled.div`
   margin: 10px 28px;
   box-sizing: content-box;
 `;
+const PanelHistogramDiv = styled.div`
+  min-width: ${(p: PanelProps) => `${p.width}px`};
+  height: ${(p: PanelProps) => `${p.height}px`};
+`;
 const PanelStat = styled.div`
   margin: 5px;
   font-size: 14px;
+`;
+const PanelPercent = styled.div`
+  height: 15px;
+  width: 100px;
+`;
+const PercentBack = styled.div`
+  width: 100px;
+  height: 10px;
+  border: 1px solid #CCD1D7;
+  border-radius: 10px;
+  position: absolute;
+`;
+interface PercentProps {
+  percent: number;
+}
+const PercentFront = styled.div`
+  width: ${(p: PercentProps) => `${p.percent}px`};
+  height: 10px;
+  background-color: #0074EB;
+  border: 1px solid #93C4F7;
+  border-radius: ${(p: PercentProps) => `10px ${p.percent > 97 ? 10 : 0}px ${p.percent > 97 ? 10 : 0}px 10px`};
+  position: absolute;
 `;
 
 interface IState {
@@ -35,6 +61,7 @@ interface IState {
 interface IProps extends IBaseProps {
   height: number;
   width: number;
+  percentComplete: number;
 }
 
 @inject("stores")
@@ -50,52 +77,67 @@ export class HistogramPanel extends BaseComponent<IProps, IState>{
   }
 
   public render() {
-    const {width, height} = this.props;
+    const {width, height, percentComplete} = this.props;
     const histogramChart = this.stores.chartsStore.charts.find(chart => chart.type === "histogram");
+
+    const data = histogramChart && histogramChart.data;
+    const threshold = kTephraThreshold;
+    let above = 0;
+    data && data.forEach((d: number) => {
+      if (d > threshold) {
+        above++;
+      }
+    });
+    const below = data ? data.length - above : 0;
+    const abovePercent = data ? Math.round(above / data.length * 100) : 0;
+    const belowPercent = data ? 100 - abovePercent : 0;
 
     return (
       <Panel height={height} width={width}>
-        { histogramChart
-          ? this.renderHistogram(histogramChart)
-          : <button onClick={this.addHistogram}>Add histogram</button>
-        }
+        <HorizontalContainer>
+          { histogramChart
+            ? this.renderHistogram(histogramChart)
+            : <PanelHistogramDiv
+                width={width - 200}
+                height={height - 10}
+              />
+          }
+          <VerticalContainer>
+            <PanelStat>
+              <PanelPercent>
+                <PercentBack/>
+                { percentComplete > 0
+                  ? <PercentFront percent={percentComplete}/>
+                  : null
+                }
+              </PanelPercent>
+              {`${percentComplete}% complete`}
+            </PanelStat>
+            <PanelStat>{`Threshold = ${threshold} mm`}</PanelStat>
+            <PanelStat>{`Count below threshold: ${below} (${belowPercent}%)`}</PanelStat>
+            <PanelStat>{`Count above threshold: ${above} (${abovePercent}%)`}</PanelStat>
+            <PanelStat>{`Risk: ${data ? this.calculateRisk(abovePercent) : "---"}`}</PanelStat>
+            { histogramChart
+              ? <button onClick={this.changeDisplayType}>Toggle display</button>
+              : <button onClick={this.addHistogram}>Add data</button>
+            }
+          </VerticalContainer>
+        </HorizontalContainer>
       </Panel>
     );
   }
 
   private renderHistogram = (chart: ChartType) => {
     const { width, height } = this.props;
-    const { data } = chart;
-
-    const threshold = kTephaThreshold;
-    let above = 0;
-    data.forEach((d: number) => {
-      if (d > threshold) {
-        above++;
-      }
-    });
-    const below = data.length - above;
-    const abovePercent = Math.round(above / data.length * 100);
-    const belowPercent = 100 - abovePercent;
-
     return (
-      <HorizontalContainer>
-        <SvgD3HistogramChart
-          width={width - 200}
-          height={height - 10}
-          chart={chart}
-          chartMin={kTephaMin}
-          chartMax={kTephaMax}
-          showBars={this.state.showBars}
-        />
-        <VerticalContainer>
-          <PanelStat>{`Threshold = ${threshold}mm`}</PanelStat>
-          <PanelStat>{`Count below threshold: ${below} (${belowPercent}%)`}</PanelStat>
-          <PanelStat>{`Count above threshold: ${above} (${abovePercent}%)`}</PanelStat>
-          <PanelStat>{`Risk: ${this.calculateRisk(abovePercent)}`}</PanelStat>
-          <button onClick={this.changeDisplayType}>Toggle display</button>
-        </VerticalContainer>
-      </HorizontalContainer>
+      <SvgD3HistogramChart
+        width={width - 200}
+        height={height - 10}
+        chart={chart}
+        chartMin={kTephraMin}
+        chartMax={kTephraMax}
+        showBars={this.state.showBars}
+      />
     );
   }
 
@@ -122,7 +164,7 @@ export class HistogramPanel extends BaseComponent<IProps, IState>{
       const title = "Chart " + (this.stores.chartsStore.charts.length + 1);
       const data: number[] = [];
       for (let i = 0; i < numPoints; i++) {
-        data.push(Math.floor(this.randomG(3) * (kTephaMax + 1)));
+        data.push(Math.floor(this.randomG(3) * (kTephraMax + 1)));
       }
       const xAxisLabel = "Tephra Thickness (mm)";
       const yAxisLabel = "Number of Runs";
