9f3208b4b9224d40069450c0708dc4e2fcb166f1
Chris Hart <chart@concord.org>
2020-09-18 17:05:58 -0400

    Simplify blocks to a single block to run the deformation simulation

--- src/blockly-blocks/deformation/block-deformation-create-sim.js
+++ src/blockly-blocks/deformation/block-deformation-create-sim.js
@@ -2,67 +2,38 @@ Blockly.Blocks['deformation-create-sim'] = {
     init: function() {
       this.appendDummyInput()
         .appendField("Create Strain Simulation");
+      this.appendDummyInput()
+        .appendField("Set velocity of Block 1 with")
+      this.appendValueInput('speed1')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("speed (mm/yr)")
+        .setCheck(['Number'])
+      this.appendValueInput('direction1')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("direction (degrees)")
+        .setCheck(['Number'])
+      this.appendDummyInput()
+        .appendField("Set velocity of Block 2 with")
+      this.appendValueInput('speed2')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("speed (mm/yr)")
+      this.appendValueInput('direction2')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("direction (degrees)")
       this.setColour(15)
       this.setPreviousStatement(false, null);
-      this.setNextStatement(true, null);
+      this.setNextStatement(false, null);
     }
   };
 
   Blockly.JavaScript['deformation-create-sim'] = function(block) {
-    const code ='createDeformationModel();\n';
-    return code;
-  }
+    var value_speed_1 = Blockly.JavaScript.valueToCode(block, 'speed1', Blockly.JavaScript.ORDER_ATOMIC) || 0;
+    var value_direction_1 = Blockly.JavaScript.valueToCode(block, 'direction1', Blockly.JavaScript.ORDER_ATOMIC) || 0;
+    var value_speed_2 = Blockly.JavaScript.valueToCode(block, 'speed2', Blockly.JavaScript.ORDER_ATOMIC) || 0;
+    var value_direction_2 = Blockly.JavaScript.valueToCode(block, 'direction2', Blockly.JavaScript.ORDER_ATOMIC) || 0;
 
-  Blockly.Blocks['set_velocity_block_1'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Set velocity Block 1 with")
-      this.appendValueInput('speed')
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("speed (mm/yr)")
-        .setCheck(['Number'])
-      this.appendValueInput('direction')
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("direction (degrees)")
-        .setCheck(['Number'])
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(240);
-      this.setTooltip("");
-      this.setHelpUrl("");
-    }
-  };
-
-  Blockly.JavaScript['set_velocity_block_1'] = function (block) {
-    var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC) || 0;
-    var value_direction = Blockly.JavaScript.valueToCode(block, 'direction', Blockly.JavaScript.ORDER_ATOMIC) || 0;
-
-    var code = `setBlockVelocity({block: 1, speed: ${value_speed}, direction: ${value_direction}});\n`;
-    return code;
-  }
-
-  Blockly.Blocks['set_velocity_block_2'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Set velocity Block 2 with")
-      this.appendValueInput('speed')
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("speed (mm/yr)")
-      this.appendValueInput('direction')
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("direction (degrees)")
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(240);
-      this.setTooltip("");
-      this.setHelpUrl("");
-    }
-  };
-
-  Blockly.JavaScript['set_velocity_block_2'] = function (block) {
-    var value_speed = Blockly.JavaScript.valueToCode(block, 'speed', Blockly.JavaScript.ORDER_ATOMIC) || 0;
-    var value_direction = Blockly.JavaScript.valueToCode(block, 'direction', Blockly.JavaScript.ORDER_ATOMIC) || 0;
-
-    var code = `setBlockVelocity({block: 2, speed: ${value_speed}, direction: ${value_direction}});\n`;
+    var code ='createDeformationModel();\n';
+    code += `setBlockVelocity({block: 1, speed: ${value_speed_1}, direction: ${value_direction_1}});\n`;
+    code += `setBlockVelocity({block: 2, speed: ${value_speed_2}, direction: ${value_direction_2}});\n`;
     return code;
   }
