36c541d9dcfca7fdaa7005f2a52b3c489421129f
Lucy Blagg <lvblagg@gmail.com>
2022-03-09 16:05:46 -0500

    added more strings to string file

--- src/strings/blockly-deformation.ts
+++ src/strings/blockly-deformation.ts
@@ -1,4 +1,5 @@
 
+// Strings for Create Deformation Simulation
 const CREATE_DEFORMATION_SIMULATION = "Create Deformation Simulation";
 const SET_VELOCITY_OF_PLATE_1_WITH = "Set velocity of Plate 1 with";
 const SPEED1 = "speed1";
@@ -8,6 +9,38 @@ const DIRECTION_DEGREES = "direction (degrees)";
 const SET_VELOCITY_OF_PLATE_2_WITH = "Set velocity of Plate 2 with";
 const SPEED2 = "speed2";
 const DIRECTION2 = "direction2";
+
+// Strings for Year Loop
+const RUN_FROM_YEAR_1 = "Run from Year 1 to Year ";
+const BY = "by";
+const YEARS = "years";
+const STEP_THROUGH = "Step through deformation model for a given number of years, with a given step size";
+
+// Strings for Model Step
+const INCREASE_DEFORMATION = "Increase Deformation by deformation build-up";
+const CALCULATED_BASED_ON = "  calculated based on...";
+const PLATE1_SPEED = "Plate 1 speed (mm/yr)";
+const DIRECTION_MODEL_1 = "direction (degrees) = 0";
+const PLATE2_SPEED = "Plate 2 speed (mm/yr)";
+const DIRECTION_MODEL_2 = "direction (degrees) = 180";
+
+// Strings for Earthquake simulation
+const TRIGGER_EARTHQUAKE = "Trigger earthquake to release energy";
+const SET_DEFORMATION = "and set Deformation to 0";
+
+// Strings for Get Deformation
+const DEFORMATION = "Deformation";
+
+// Strings for Get Max Deformation
+const MAX_DEFORMATION = "Max deformation calculated based on";
+const LOW = "low";
+const MEDIUM = "medium";
+const HIGH = "high";
+const FRICTION = "friction";
+
+// Strings for Boundary Orientation
+const ORIENTATION = "orientation";
+const SET_BOUNDARY = "Set boundary orientation";
 
 module.exports = {
     CREATE_DEFORMATION_SIMULATION,
