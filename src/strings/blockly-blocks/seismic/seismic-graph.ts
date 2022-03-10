547e11e8959a6d75bd469f3da18a217fe5f449e5
lublagg <lvblagg@gmail.com>
2022-03-10 15:25:20 -0500

    extracted strings from authoring menu

--- src/strings/blockly-blocks/seismic/seismic-graph.ts
+++ src/strings/blockly-blocks/seismic/seismic-graph.ts
@@ -1,9 +1,9 @@
 // Graph GPS position
-const GRAPH_GPS = 'Graph GPS location over time';
-const STATION = 'Station';
-const START_DATE = 'Start date';
-const END_DATE = 'End date';
-const DURATION = 'Duration (days)';
+const GRAPH_GPS = "Graph GPS location over time";
+const STATION = "Station";
+const START_DATE = "Start date";
+const END_DATE = "End date";
+const DURATION = "Duration (days)";
 
 module.exports = {
     GRAPH_GPS,
---
+++ src/strings/components/authoring-menu-strings.ts
@@ -0,0 +1,69 @@
+// model options
+export const MODEL_OPTIONS = "Model options";
+export const UNIT = "Unit";
+
+//// chart names ////
+export const TEPHRA = "Tephra";
+export const SEISMIC = "Seismic";
+
+//// shared menu options ////
+export const CODE_TOOLBOX = "Code toolbox";
+export const INITIAL_CODE = "Initial code";
+
+// left tabs
+export const LEFT_TABS = "Left Tabs";
+export const SHOW_BLOCKS = "Show blocks?";
+export const SHOW_CODE = "Show code?";
+export const SHOW_CONTROLS = "Show controls?";
+
+// right tabs
+export const RIGHT_TABS = "Right Tabs";
+export const SHOW_CONDITIONS = "Show conditions?";
+export const SHOW_DATA = "Show data?";
+
+// additional options
+export const SHOW_SPEED_CONTROLS = "Show Speed Controls?";
+
+// state options
+export const SAVE_STATE = "Save current state to local storage";
+export const LOAD_STATE = "Load state from local storage";
+
+//// tephra menu ////
+export const REQUIRE_ERUPTION = "Require eruption?";
+export const REQUIRE_PAINTING = "Require painting?";
+export const MAP_SCENARIO = "Map Scenario";
+
+// right tab options
+export const SHOW_MONTE_CARLO = "Show monte carlo?";
+export const SHOW_CROSS_SECTION = "Show cross section?";
+
+// control options
+export const CONTROL_OPTIONS = "Controls Options";
+export const SHOW_WIND_SPEED = "Show Wind Speed?";
+export const SHOW_WIND_DIRECTION = "Show Wind Direction?";
+export const SHOW_EJECTED_VOLUME = "Show Ejected Volume?";
+export const SHOW_COLUMN_HEIGHT = "Show Column Height?";
+export const SHOW_VEI = "Show VEI?";
+
+export const SHOW_HISTOGRAM = "Show Bar Histogram?";
+export const SHOW_DEMO_CHARTS = "Show Demo Charts?";
+export const SHOW_RISK_DIAMONDS = "Show Risk Diamonds?";
+
+//// seismic menu ////
+
+// right tab options
+export const SHOW_MAP = "Show map?";
+export const SHOW_DEFORMATION_Q = "Show deformation?";
+
+// deformation model options
+export const DEFORMATION_MODEL = "Deformation Model";
+export const MODEL_WIDTH = "Model width (km)";
+export const APPARENT_WIDTH = "Apparent width (km)";
+export const APPARENT_YEAR = "Apparent year scale";
+export const SHOW_YEARS = "Show years?";
+export const LOW_FRICTION = "Max displ. Low friction";
+export const MED_FRICTION = "Max displ. Med friction";
+export const HIGH_FRICTION = "Max displ. High friction";
+export const RAINBOW_LINES = "Rainbow lines?";
+export const FAULT_ANGLE = "Fault Angle (º)";
+export const EARTHQUAKES = "Earthquakes";
