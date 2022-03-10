71cafbb5168f09e311c9b86595b711eaa0299000
Lucy Blagg <lvblagg@gmail.com>
2022-03-10 10:09:36 -0500

    extracted strings from seismic graph gps position block

---
+++ src/strings/blockly-blocks/seismic/seismic-graph.ts
@@ -0,0 +1,14 @@
+// Graph GPS position
+const GRAPH_GPS = 'Graph GPS location over time';
+const STATION = 'Station';
+const START_DATE = 'Start date';
+const END_DATE = 'End date';
+const DURATION = 'Duration (days)';
+
+module.exports = {
+    GRAPH_GPS,
+    STATION,
+    START_DATE,
+    END_DATE,
+    DURATION
+};
