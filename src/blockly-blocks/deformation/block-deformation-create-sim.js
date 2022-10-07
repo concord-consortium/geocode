d30bc62f28b4711c0b5dd7ec26d54c9b806614b6
lublagg <lvblagg@gmail.com>
2022-10-07 11:34:29 -0400

    Moved "Run from year..." block to Logic category.

--- src/blockly-blocks/deformation/block-deformation-create-sim.js
+++ src/blockly-blocks/deformation/block-deformation-create-sim.js
@@ -57,39 +57,6 @@ Blockly.Blocks['deformation-create-sim'] = {
     return `createDeformationGraph();`;
   }
 
-  Blockly.Blocks['deformation-year-loop'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField(strings.RUN_FROM_YEAR_1)
-          .appendField(new Blockly.FieldNumber(500, 0, 500), "max_year")
-          .appendField(strings.BY)
-          .appendField(new Blockly.FieldDropdown([["1","1"], ["10","10"], ["20","20"]]), "year_step")
-          .appendField(strings.YEARS);
-      this.appendStatementInput("DO")
-          .setCheck(null);
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour("#B35F00")
-      this.setTooltip(strings.STEP_THROUGH);
-      this.setHelpUrl("");
-    }
-  };
-
-  Blockly.JavaScript['deformation-year-loop'] = function(block) {
-    var number_max_year = block.getFieldValue('max_year');
-
-    var dropdown_year_step = block.getFieldValue('year_step');
-    var branch = Blockly.JavaScript.statementToCode(block, 'DO');
-
-    var code = 'createNewRun();\n';
-
-    code += 'for (var year = ' + dropdown_year_step + '; ' +
-        'year <= ' + number_max_year + '; ' +
-        'year += ' + dropdown_year_step + ') {\n' +
-        branch +
-        '};\n';
-    return code;
-  };
 
   Blockly.Blocks['deformation-model-step'] = {
     init: function() {
