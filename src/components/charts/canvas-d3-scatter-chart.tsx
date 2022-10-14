674255ff14add5b7bd3a9c13c17fadb9897862bf
lublagg <lvblagg@gmail.com>
2022-10-14 13:32:37 -0400

    Cleaned up code.

--- src/components/charts/canvas-d3-scatter-chart.tsx
+++ src/components/charts/canvas-d3-scatter-chart.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as d3 from "d3";
 import { ChartType } from "../../stores/charts-store";
 import { addFadeLegend, getFadeColor } from "./svg-d3-scatter-chart";
+import { format } from "mathjs";
 
 type Scale = d3.ScaleLinear<number, number> | d3.ScaleTime<number, number>;
 
@@ -59,9 +60,8 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
     const yRange = Number(chart.extent(1)[1]) - Number(chart.extent(1)[0]);
 
     let chartWidth = width - margin.left - margin.right + (canvasPadding * 2);
-    if (xAxisLabel2) {
-      chartWidth -= 200;
-    }
+    // accommodate extra space for labels on left and right sides
+    if (xAxisLabel2) chartWidth -= 200;
 
     // adjust height if the x and y axes need to be scaled uniformly, base off of width
     const chartHeight = uniformXYScale
@@ -69,14 +69,11 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
       : height - margin.top - margin.bottom + (canvasPadding * 2);
 
     let usedHeight = height;
-
     if (uniformXYScale) {
       usedHeight = chartHeight + margin.top + margin.bottom + (canvasPadding * 2);
-      if (yAxisLabel2) {
-        usedHeight += 200;
-      }
+      // accommodate extra space for labels on top and bottom
+      if (yAxisLabel2) usedHeight += 50;
     }
-
     return { width, height: usedHeight, chartWidth, chartHeight};
   }
 
@@ -86,22 +83,17 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
     const { chart } = this.props;
     const { data, xAxisLabel, yAxisLabel, fadeIn, gridlines, dataOffset, uniformXYScale,
       xAxisLabel2, yAxisLabel2 } = chart;
+    const hasFourAxisLabels = !!xAxisLabel && !!yAxisLabel && !!xAxisLabel2 && !!yAxisLabel2;
     const chartDimensions = this.calculateChartDimensions();
     const { width, height, chartWidth, chartHeight } = chartDimensions;
-    const hasFourAxisLabels = xAxisLabel && yAxisLabel && xAxisLabel2 && xAxisLabel2.length && yAxisLabel2 && yAxisLabel2.length;
 
     const xRange = Number(chart.extent(0)[1]) - Number(chart.extent(0)[0]);
     const yRange = Number(chart.extent(1)[1]) - Number(chart.extent(1)[0]);
     const xUniformTicks = Math.floor(xRange / 100);
     const yUniformTicks = Math.floor(yRange / 100);
 
-    let marginLeft = margin.left;
-    let marginTop = margin.top;
-
-    if (hasFourAxisLabels) {
-      marginLeft += 100;
-      marginTop += 50;
-    }
+    const marginLeft = hasFourAxisLabels ? (margin.left + 75) : margin.left;
+    const marginTop = hasFourAxisLabels ? (margin.top + 50) : margin.top;
 
     const svgAxes = d3.select(this.svgRef.current)
       .attr("width", width)
@@ -132,6 +124,7 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
         .attr("class", "grid x")
         .attr("transform", "translate(0," + chartHeight + ")")
         .style("stroke", "#C0C0C0")
+        .style("stroke-opacity", ".5")
         .call(make_x_gridlines()
             .tickSize(-chartHeight)
             .tickFormat((d) => "")
@@ -141,78 +134,75 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
       svgAxes.append("g")
         .attr("class", "grid y")
         .style("stroke", "#C0C0C0")
+        .style("stroke-opacity", ".5")
         .call(make_y_gridlines()
             .tickSize(-chartWidth)
             .tickFormat((d) => "")
         );
 
-       d3.selectAll(".grid").selectAll(".domain").style("opacity", "0");
+      if (hasFourAxisLabels) {
+        d3.selectAll(".grid").selectAll(".domain").style("opacity", "0");
 
-      const ticks = d3.selectAll(".grid").selectAll(".tick");
-      ticks
-        .style("stroke-width", (d, idx) => d === 0 ? "5px" : d === 700 || d === -600 ? "0px" : "1px")
-        .style("stroke-opacity", (d) => d === 0 ? "1" : ".5");
+        const ticks = d3.selectAll(".grid").selectAll(".tick");
+        ticks.style("stroke-opacity", (d) => d === 0 ? "1" : ".5");
 
-      const blah = (idx: number) => {
-        if (idx === 0 || idx === 8) {
-          return "0px"
-        } else {
-          return "1px";
-        }
+        const getStrokeWidth = (d: unknown, idx: number, numTicks: number) => {
+          return d === 0 ? "5px" : idx === 0 || idx === numTicks ? "0px" : "1px";
+        };
+
+        d3.selectAll(".grid.x").selectAll(".tick")
+          .style("stroke-width", (d, idx) => getStrokeWidth(d, idx, xUniformTicks));
+        d3.selectAll(".grid.y").selectAll(".tick")
+          .style("stroke-width", (d, idx) =>  getStrokeWidth(d, idx, yUniformTicks));
       }
-
-      const blah2 = (idx: number) => {
-        if (idx === 0 || idx === 7) {
-          return "0px"
-        } else {
-          return "1px";
-        }
-      }
-
-      d3.selectAll(".grid.y").selectAll(".tick")
-        .style("stroke-width", (d, idx) => d === 0 ? "5px" : blah(idx))
-      d3.selectAll(".grid.x").selectAll(".tick")
-        .style("stroke-width", (d, idx) => d === 0 ? "5px" : blah2(idx))
     }
 
     // add axes
-    const axisBottom = chart.isDate(0) ?
-        d3.axisBottom(xScale).tickFormat((date: Date) => {
-          // remove last "Jan" from Time of Year chart
-          if (chart.dateLabelFormat === "%b" && date.getFullYear() === 1901) return "";
-          return chart.toDateString()(date);
-        }) :
-        uniformXYScale ?
-        d3.axisBottom(xScale).ticks(xUniformTicks).tickSize(0).tickFormat((d) => Number(d) < 0 ? `${(Number(d) * (-1))}` : `${d}`) :
-        d3.axisBottom(xScale).ticks;
+    let axisBottom;
+    let xAxisTranslation;
+    let axisLeft;
+    let yAxisTranslation;
 
+    if (hasFourAxisLabels) {
+      const formatTick = (d: unknown) => Number(d) < 0 ? `${(Number(d) * (-1))}` : `${d}`;
+
+      axisBottom = d3.axisBottom(xScale).ticks(xUniformTicks).tickSize(0).tickFormat((d) => formatTick(d))
+      xAxisTranslation = `(0, ${(yScale(0)! + 10)})`;
+
+      axisLeft = d3.axisLeft(yScale).ticks(yUniformTicks).tickSize(0).tickFormat((d) => formatTick(d));
+      yAxisTranslation = `translate(${xScale(0)! - 10}, 0)`
+    } else {
+      axisBottom = chart.isDate(0) ?
+          d3.axisBottom(xScale).tickFormat((date: Date) => {
+            // remove last "Jan" from Time of Year chart
+            if (chart.dateLabelFormat === "%b" && date.getFullYear() === 1901) return "";
+            return chart.toDateString()(date);
+          }) :
+          uniformXYScale ? d3.axisBottom(xScale).ticks(xUniformTicks) : d3.axisBottom(xScale).ticks;
+      xAxisTranslation = `(0, ${chartHeight})`;
+
+      axisLeft = chart.isDate(1) ?
+        d3.axisLeft(yScale).tickFormat(chart.toDateString()) :
+        uniformXYScale ? d3.axisLeft(yScale).ticks(yUniformTicks): d3.axisLeft(yScale);
+      yAxisTranslation = "none";
+    }
     svgAxes.append("g")
-      .attr("transform", "translate(0," + (yScale(0)! + 10) + ")")
-      .attr("class", "x-axis")
+      .attr("transform", `translate${xAxisTranslation}`)
+      .attr("class", "bottom axis")
       .call(axisBottom);
 
-
-
-    const axisLeft = chart.isDate(1) ?
-      d3.axisLeft(yScale).tickFormat(chart.toDateString()) :
-      uniformXYScale ? d3.axisLeft(yScale).ticks(yUniformTicks).tickSize(0).tickFormat((d) => Number(d) < 0 ? `${(Number(d) * (-1))}` : `${d}`) : d3.axisLeft(yScale);
-
     svgAxes.append("g")
-      .attr("transform", `translate(${xScale(0)! - 10}, 0)`)
-      .attr("class", "y-axis")
+      .attr("transform", yAxisTranslation)
+      .attr("class", "left axis")
       .call(axisLeft)
 
-    d3.select(".x-axis")
-      .select("path")
-        .style("transform", "translate(0, 5px)")
-        .style("stroke-width", "25px")
-        .style("stroke", "#fff")
+    console.log(`d3.selectAll(".bottom.axis").selectAll("path")`, d3.selectAll(".bottom.axis").selectAll("path"));
 
-    d3.select(".y-axis")
-      .select("path")
-        .style("transform", "translate(-12px, 0)")
-        .style("stroke-width", "25px")
-        .style("stroke", "#fff")
+    if (hasFourAxisLabels) {
+      d3.selectAll(".axis").selectAll("path").style("stroke-width", "25px").style("stroke", "#fff");
+      d3.selectAll(".bottom.axis").selectAll("path").attr("transform", "translate(0, 5)");
+      d3.selectAll(".left.axis").selectAll("path").attr("transform", "translate(-12, 0)");
+    }
 
     // Add labels
     if (!hasFourAxisLabels) {
@@ -236,55 +226,38 @@ export class CanvasD3ScatterChart extends React.Component<IProps> {
           .text(yAxisLabel);
       }
   } else {
-    // add additional labels
-    const northLabel = d3.axisTop(xScale).ticks(xUniformTicks).tickSize(0).tickFormat((d) => d === 0 ? yAxisLabel : "");
-    svgAxes.append("g")
-      .attr("class", "axes-labels")
-      .attr("transform", "translate(0," + -20 + ")")
-      .call(northLabel)
+    const axesForLabels = [d3.axisTop, d3.axisBottom, d3.axisLeft, d3.axisRight];
+    const scales = [xScale, xScale, yScale, yScale];
+    const ticks = [xUniformTicks, xUniformTicks, yUniformTicks, yUniformTicks];
+    const labelText = [yAxisLabel, yAxisLabel2, xAxisLabel2, xAxisLabel];
+    const labelTranslations = [`(0, -20)`, `(0, ${(chartHeight + 10)})`, `(0, 0)`, `(${chartWidth}, 0)`];
 
-    const southLabel = d3.axisBottom(xScale).ticks(xUniformTicks).tickSize(0).tickFormat((d) => d === 0 ? yAxisLabel2 : "");
-    svgAxes.append("g")
-      .attr("class", "axes-labels")
-      .attr("transform", "translate(0," + (chartHeight + 30) + ")")
-      .call(southLabel);
+    for (let i = 0; i < axesForLabels.length; i ++) {
+      const label = axesForLabels[i](scales[i]).ticks(ticks[i]).tickSize(0).tickFormat((d) => d === 0 ? labelText[i] : "");
+      svgAxes.append("g")
+        .attr("class", "axes-labels")
+        .attr("transform", `translate${labelTranslations[i]}`)
+        .call(label)
+    }
 
-    const westLabel = d3.axisLeft(yScale).ticks(yUniformTicks).tickSize(0).tickFormat((d) => d === 0 ? xAxisLabel2 : "");
-    svgAxes.append("g")
-      .attr("class", "axes-labels")
-      .call(westLabel);
+    const axesLabels = d3.selectAll("g.axes-labels");
+    axesLabels.selectAll("path").style("opacity", "0");
+    axesLabels.style("font-size", "0.9em").style("font-weight", "bold");
 
-    const eastLabel = d3.axisRight(yScale).ticks(yUniformTicks).tickSize(0).tickFormat((d) => d === 0 ? xAxisLabel : "");
-    svgAxes.append("g")
-      .attr("class", "axes-labels")
-      .attr("transform", "translate(0" + chartWidth + ")")
-      .call(eastLabel)
-
-    d3.selectAll("g.axes-labels")
-      .selectAll("g.tick")
-      .filter((d) => d === 0)
-      .append("g")
+    axesLabels.selectAll("g.tick").filter((d) => d === 0).append("g")
         .attr("class", "mm-label")
         .append("text")
           .style("fill", "#555")
-          .html("(mm)")
+          .text("(mm)")
 
-    d3.selectAll("g.axes-labels").selectAll("path").style("opacity", "0");
-    d3.selectAll("g.axes-labels").style("font-size", "0.9em").style("font-weight", "bold");
-
-    const translations = ["10px, 10px", "0px, 30px", "-35px, 20px", "40px, 20px"]
-
-    const getTranslationValue = (idx: number) => {
-      return `translate(${translations[idx]})`
-    };
-
-    d3.selectAll(".mm-label")
-      .style("transform", (d, idx) => getTranslationValue(idx))
+    const mmTranslations = ["10px, 10px", "0px, 30px", "-35px, 20px", "40px, 20px"]
+    const getMMTranslationValue = (idx: number) => `translate(${mmTranslations[idx]})`;
+    d3.selectAll(".mm-label").style("transform", (d, idx) => getMMTranslationValue(idx))
   }
 
     d3.select(this.canvasRef.current)
       .attr("width", chartWidth + (canvasPadding * 2))
-      .attr("height", height + (canvasPadding * 2))
+      .attr("height", chartHeight + (canvasPadding * 2))
       .style("margin-left", marginLeft - canvasPadding + "px")
       .style("margin-top", marginTop - canvasPadding + "px");
 
