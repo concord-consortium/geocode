56c8e346d63c3e08528fea3929f5d6726bac9bd0
Kirk Swenson <kswenson@users.noreply.github.com>
2025-04-24 13:41:05 -0700

    chore: replace cypress-image-snapshot with @simonsmith/cypress-image-snapshot

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress"
-import { addMatchImageSnapshotPlugin } from "cypress-image-snapshot/plugin"
+import { addMatchImageSnapshotPlugin } from "@simonsmith/cypress-image-snapshot/plugin"
 const fs = require('fs-extra');
 const path = require('path');
 
