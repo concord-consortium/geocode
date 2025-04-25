569a33b100a710e91648514f9cc5023ebaf83c10
Kirk Swenson <kswenson@users.noreply.github.com>
2025-04-24 17:05:13 -0700

    chore: replace enzyme with @testing-library/react

--- src/components/buttons/run-button.test.tsx
+++ src/components/buttons/run-button.test.tsx
@@ -1,28 +1,32 @@
-import * as React from "react";
+import { render } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
 import RunButtons from "./run-buttons";
-import { shallow } from "enzyme";
 
 describe("RunButtons component", () => {
   it("renders all the buttons", () => {
     const nullfunc = () => null;
-    const wrapper = shallow(<RunButtons
-      run={nullfunc}
-      stop={nullfunc}
-      step={nullfunc}
-      reset={nullfunc}
-      pause={nullfunc}
-      unpause={nullfunc}
-      reload={nullfunc}
-      setSpeed={nullfunc}
-      isAtInitialState={false}
-      running={false}
-      paused={false}
-      showSpeedControls={false}
-      speed={0}
-    />);
-    expect(wrapper.contains("<RunButton />"));
-    expect(wrapper.contains("<StopButton />"));
-    expect(wrapper.contains("<StepButton />"));
-    expect(wrapper.contains("<ResetButton />"));
+
+    const { container } = render(
+      <RunButtons
+        run={nullfunc}
+        stop={nullfunc}
+        step={nullfunc}
+        reset={nullfunc}
+        pause={nullfunc}
+        unpause={nullfunc}
+        reload={nullfunc}
+        setSpeed={nullfunc}
+        isAtInitialState={false}
+        running={false}
+        paused={false}
+        showSpeedControls={false}
+        speed={0}
+      />
+    );
+
+    expect(screen.getByText("Run")).toBeInTheDocument();
+    expect(screen.getByText("Step")).toBeInTheDocument();
+    expect(screen.getByText("Reset")).toBeInTheDocument();
+    expect(screen.getByText("Reload")).toBeInTheDocument();
   });
 });
