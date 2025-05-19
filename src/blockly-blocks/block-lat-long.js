30621be453fcef168a9ed26c6f2752e4ae3091d7
Teale Fristoe <fristoe@gmail.com>
2025-05-19 09:47:20 -0700

    Better error checking for lat/long blocks.

--- src/blockly-blocks/block-lat-long.js
+++ src/blockly-blocks/block-lat-long.js
@@ -9,17 +9,27 @@ Blockly.Blocks.lat_long = {
     this.setHelpUrl('');
   }
 };
+
+const failCode = [`({lat: null, long: null})`, Blockly.JavaScript.ORDER_NONE];
+
 Blockly.JavaScript.lat_long = function (block) {
   const lat_long = block.getFieldValue('lat_long');
 
   try {
     const [lat, long] = lat_long.split(",").map(str => str.trim()).map(Number);
+
+    // TODO: Handle lat/long ranges
+    if (isNaN(lat) || isNaN(long)) {
+      console.error("Latitude and longitude values must be numbers");
+      return failCode;
+    }
+
     // have to wrap in parens or the parser doesn't like it as a fragment
     const code = `({lat: ${lat}, long: ${long}})`;
     // TODO: Change ORDER_NONE to the correct strength.
     return [code, Blockly.JavaScript.ORDER_NONE];
   } catch (e) {
     console.error("Error parsing lat_long:", e);
-    return [`({lat: null, long: null})`, Blockly.JavaScript.ORDER_NONE];
+    return failCode;
   }
 };
