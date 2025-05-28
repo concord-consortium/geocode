472b3c8cac8f7b2e10bed79ee55523afb5c83047
Kirk Swenson <kswenson@users.noreply.github.com>
2025-05-28 11:35:35 -0700

    feat: implement rotation of camera heading and pitch

---
+++ src/components/lava-coder/lava-icon-button.tsx
@@ -0,0 +1,20 @@
+import IconButton, { IProps as IIconButtonProps } from "../buttons/icon-button";
+
+interface IProps extends Partial<IIconButtonProps> {
+  isActive?: boolean;
+}
+
+export function LavaIconButton(inProps: IProps) {
+  const props: IProps = {
+    ...inProps,
+    height: 34,
+    width: 34,
+    hoverColor: "#b7dcad",
+    activeColor: "#cee6c9",
+    borderColor: "#3baa1d"
+
+  };
+  return (
+    <IconButton {...props} />
+  );
+}
--- src/components/lava-coder/use-camera-controls.ts
+++ src/components/lava-coder/use-camera-controls.ts
@@ -1,21 +1,89 @@
-import { Cartesian3, Cartographic, CesiumWidget, Math as CSMath, sampleTerrainMostDetailed } from "@cesium/engine";
-import { useEffect } from "react";
+import {
+  Cartesian2, Cartesian3, Cartographic, CesiumWidget, Math as CSMath, HeadingPitchRange, sampleTerrainMostDetailed
+} from "@cesium/engine";
+import { useCallback, useEffect, useState } from "react";
+import { IOnDragArgs, useCesiumDragEvents } from "./use-cesium-drag-events";
+
+export const kDefaultCameraMode = "panning";
 
 const kDefaultXLng = -155.470;
 const kDefaultYLat = 19.150;
-const kMinDistanceAboveTerrain = 500;
-const kMaxDistanceAboveTerrain = 130000;
+const kMinDistanceAboveTerrain = 1000;
+const kMaxDistanceAboveTerrain = 135000;
+
+function getAngleFromCenter(pos: Cartesian2, center: Cartesian2) {
+  return Math.atan2(pos.y - center.y, pos.x - center.x);
+}
 
 export function useCameraControls(viewer: CesiumWidget | null, verticalExaggeration: number) {
 
-  function cameraMoveRate() {
+  const [cameraMode, setCameraMode] = useState<"pitch" | "heading" | "panning">(kDefaultCameraMode);
+
+  const handlePan = useCallback(({ dx, dy }: IOnDragArgs) => {
+    if (!viewer) return;
+
+    const { camera } = viewer;
+
+    // Adjust the sensitivity as needed
+    const panFactor = 0.5;
+
+    // Pan the camera: moveRight and moveUp are in meters, so you may want to scale by camera height
+    const moveRate = camera.positionCartographic.height / 500 * panFactor;
+    camera.moveRight(-dx * moveRate);
+    camera.moveUp(dy * moveRate);
+  }, [viewer]);
+
+  const handleRotateHeading = useCallback(({ startPosition, position, initialCameraState }: IOnDragArgs) => {
+    if (!viewer) return;
+
+    const { camera } = viewer;
+    const { center, target, initialHeading, initialPitch, initialRange } = initialCameraState;
+
+    const prevAngle = getAngleFromCenter(startPosition, center);
+    const currAngle = getAngleFromCenter(position, center);
+    const deltaTheta = currAngle - prevAngle; // Radians, positive is CCW
+
+    camera.lookAt(target, new HeadingPitchRange(
+      initialHeading - deltaTheta,
+      initialPitch,
+      initialRange
+    ));
+  }, [viewer]);
+
+  const handleRotatePitch = useCallback(({ dyTotal, initialCameraState }: IOnDragArgs) => {
+    if (!viewer) return;
+
+    const { camera } = viewer;
+    const { target, initialHeading, initialPitch, initialRange } = initialCameraState;
+
+    const pitchFactor = 0.01;
+
+    camera.lookAt(target, new HeadingPitchRange(
+      initialHeading,
+      // -89 to avoid discontinuity at -90 degrees pitch
+      CSMath.clamp(initialPitch - dyTotal * pitchFactor, CSMath.toRadians(-89), CSMath.toRadians(-15)),
+      initialRange
+    ));
+  }, [viewer]);
+
+  const handleDrag = cameraMode === "panning"
+                      ? handlePan
+                      : cameraMode === "heading"
+                        ? handleRotateHeading
+                        : cameraMode === "pitch"
+                          ? handleRotatePitch
+                          : undefined;
+
+  useCesiumDragEvents(viewer, handleDrag);
+
+  function cameraZoomRate() {
     if (!viewer) return;
     const cameraPos = viewer.camera.positionCartographic;
     return cameraPos.height / 5;
   }
 
   async function zoomIn() {
-    let moveDist = cameraMoveRate();
+    let moveDist = cameraZoomRate();
     if (!viewer || !moveDist) return;
 
     // Sample the terrain at the camera's lat/lon
@@ -34,7 +102,7 @@ export function useCameraControls(viewer: CesiumWidget | null, verticalExaggerat
   }
 
   function zoomOut() {
-    let moveDist = cameraMoveRate();
+    let moveDist = cameraZoomRate();
     if (!viewer || !moveDist) return;
 
     const cameraPos = viewer.camera.positionCartographic;
@@ -58,9 +126,13 @@ export function useCameraControls(viewer: CesiumWidget | null, verticalExaggerat
       });
 
       // Disable the default camera controls
+      viewer.scene.screenSpaceCameraController.enableLook = false;
+      viewer.scene.screenSpaceCameraController.enableRotate = false;
+      viewer.scene.screenSpaceCameraController.enableTilt = false;
+      viewer.scene.screenSpaceCameraController.enableTranslate = false;
       viewer.scene.screenSpaceCameraController.enableZoom = false;
     }
   }, [viewer]);
 
-  return { zoomIn, zoomOut };
+  return { cameraMode, setCameraMode, zoomIn, zoomOut };
 }
