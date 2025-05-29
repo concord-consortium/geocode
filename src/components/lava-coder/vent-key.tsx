ee6daed84ea4ddc9b296edaf6ad420ad7db5bc0f
Teale Fristoe <fristoe@gmail.com>
2025-05-29 16:12:34 -0700

    Add close button.

--- src/components/lava-coder/vent-key.tsx
+++ src/components/lava-coder/vent-key.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CloseIcon from "../../assets/lava-coder/close-icon@3x.png";
 import VentKeyIcon from "../../assets/lava-coder/key-icon@3x.png";
 import { LavaIconButton } from "./lava-icon-button";
 
@@ -10,29 +11,32 @@ export function VentKey() {
   return (
     <div className="vent-key-container">
       {!displayKey && (
-        <div className="lava-overlay-controls-top top-right-controls">
-          <LavaIconButton className="show-vent-key-button" label="Key" width={24}
-                          onClick={() => setDisplayKey(prev => !prev)}>
-            <img src={VentKeyIcon} alt="Show Vent Key" className="vent-key-icon" />
-          </LavaIconButton>
-        </div>
+        <LavaIconButton className="show-vent-key-button" label="Key" width={24}
+                        onClick={() => setDisplayKey(true)}>
+          <img src={VentKeyIcon} alt="Show Vent Key" className="vent-key-icon" />
+        </LavaIconButton>
       )}
 
       {displayKey && (
-        <div className="vent-key">
-          <div className="description">
-            <div className="description-line">Likelihood</div>
-            <div className="description-line">of Vent Location</div>
+        <>
+          <button className="close-button" onClick={() => setDisplayKey(false)}>
+            <img src={CloseIcon} alt="Close Key" className="close-icon" />
+          </button>
+          <div className="vent-key">
+            <div className="description">
+              <div className="description-line">Likelihood</div>
+              <div className="description-line">of Vent Location</div>
+            </div>
+            <div className="item">
+              <div className="item-box very-likely" />
+              <div className="item-label">Very Likely</div>
+            </div>
+            <div className="item">
+              <div className="item-box likely" />
+              <div className="item-label">Likely</div>
+            </div>
           </div>
-          <div className="item">
-            <div className="item-box very-likely" />
-            <div className="item-label">Very Likely</div>
-          </div>
-          <div className="item">
-            <div className="item-box likely" />
-            <div className="item-label">Likely</div>
-          </div>
-        </div>
+        </>
       )}
     </div>
   );
