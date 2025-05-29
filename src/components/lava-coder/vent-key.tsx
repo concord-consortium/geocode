e77559df7b2212620033731efc987e8af51d6041
Teale Fristoe <fristoe@gmail.com>
2025-05-29 15:58:14 -0700

    Move key button into VentKey component.

--- src/components/lava-coder/vent-key.tsx
+++ src/components/lava-coder/vent-key.tsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
+import VentKeyIcon from "../../assets/lava-coder/key-icon@3x.png";
+import { LavaIconButton } from "./lava-icon-button";
+
 import "./vent-key.scss";
 
 export function VentKey() {
+  const [displayKey, setDisplayKey] = useState(false);
+
   return (
     <div className="vent-key-container">
-      <div className="description">
-        <div className="description-line">Likelihood</div>
-        <div className="description-line">of Vent Location</div>
-      </div>
-      <div className="item">
-        <div className="item-box very-likely" />
-        <div className="item-label">Very Likely</div>
-      </div>
-      <div className="item">
-        <div className="item-box likely" />
-        <div className="item-label">Likely</div>
-      </div>
+      {!displayKey && (
+        <div className="lava-overlay-controls-top top-right-controls">
+          <LavaIconButton className="show-vent-key-button" label="Key" width={24}
+                          onClick={() => setDisplayKey(prev => !prev)}>
+            <img src={VentKeyIcon} alt="Show Vent Key" className="vent-key-icon" />
+          </LavaIconButton>
+        </div>
+      )}
+
+      {displayKey && (
+        <div className="vent-key">
+          <div className="description">
+            <div className="description-line">Likelihood</div>
+            <div className="description-line">of Vent Location</div>
+          </div>
+          <div className="item">
+            <div className="item-box very-likely" />
+            <div className="item-label">Very Likely</div>
+          </div>
+          <div className="item">
+            <div className="item-box likely" />
+            <div className="item-label">Likely</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
