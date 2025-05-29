5b101c647474171aea51a0355c0a7e3cba42a748
Kirk Swenson <kswenson@users.noreply.github.com>
2025-05-29 13:35:01 -0700

    feat: use low-resolution imagery during development [GEOCODE-49] (#381)

--- src/components/lava-coder/use-cesium-viewer.ts
+++ src/components/lava-coder/use-cesium-viewer.ts
@@ -1,26 +1,39 @@
-import { CesiumWidget, Ion } from "@cesium/engine";
+import { CesiumWidget, ImageryLayer, Ion } from "@cesium/engine";
 import { useEffect, useRef, useState } from "react";
+import { LavaMapType } from "../../stores/ui-store";
 import { useTerrainProvider } from "./use-terrain-provider";
+import { useWorldImagery } from "./use-world-imagery";
 
 import "@cesium/engine/Source/Widget/CesiumWidget.css";
 
 Ion.defaultAccessToken = process.env.CESIUM_ION_ACCESS_TOKEN;
 
-export function useCesiumViewer(container: Element | null) {
+export function useCesiumViewer(container: Element | null, initialMapType: LavaMapType) {
   const viewer = useRef<CesiumWidget | null>(null);
   const [ , forceRefresh] = useState(false);
+  const { createBaseLayer } = useWorldImagery();
+  const [baseLayer, setBaseLayer] = useState<ImageryLayer | null>(null);
   const terrainProvider = useTerrainProvider();
 
   useEffect(() => {
-    if (container && terrainProvider && !viewer.current) {
+    if (!baseLayer) {
+      createBaseLayer(initialMapType).then(layer => {
+        setBaseLayer(layer);
+      });
+    }
+  }, [baseLayer, createBaseLayer, initialMapType]);
+
+  useEffect(() => {
+    if (!viewer.current && container && baseLayer && terrainProvider) {
       viewer.current = new CesiumWidget(container, {
         shouldAnimate: true,
+        baseLayer,
         terrainProvider
       });
       forceRefresh(prev => !prev);
     }
     return () => viewer.current?.destroy();
-  }, [container, terrainProvider]);
+  }, [baseLayer, container, terrainProvider]);
 
   return viewer.current;
 }
