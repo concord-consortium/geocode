1a10047cecc13d94a538c63b8441d2e4148b76f4
Teale Fristoe <fristoe@gmail.com>
2025-08-26 14:51:28 -0700

    Cypress debugging.

--- cypress/e2e/branch/molasses.cy.js
+++ cypress/e2e/branch/molasses.cy.js
@@ -7,10 +7,10 @@ const modelOptions = new ModelOptions();
 
 // need to convert Blockly strings because they have &nbsp; in the DOM
 // but when run thru Cypress, Cypress converts it so `contain` never matches
-function removeNBSP(text){
-    const re = new RegExp(String.fromCharCode(160), "g");
-    return text.replace(re, " ");
-}
+// function removeNBSP(text){
+//     const re = new RegExp(String.fromCharCode(160), "g");
+//     return text.replace(re, " ");
+// }
 
 context("Molasses Simulation", () => {
   beforeEach(() => {
@@ -33,9 +33,9 @@ context("Molasses Simulation", () => {
       blocksTab.getTag('Volcano').click();
       blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
       blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 17);
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(0).text().then((text) => {
-          expect(removeNBSP(text)).to.containIgnoreCase("Compute and visualize lava flow with...");
-      });
+      // blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(0).text().then((text) => {
+      //     expect(removeNBSP(text)).to.containIgnoreCase("Compute and visualize lava flow with...");
+      // });
     //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(1).text().then((text) => {
     //       expect(removeNBSP(text)).to.containIgnoreCase("Set eruption volume");
     //   });
