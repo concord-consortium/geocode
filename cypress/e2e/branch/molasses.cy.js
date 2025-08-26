8040eb0c27d8122530d22d135152211c077fdbc4
Teale Fristoe <fristoe@gmail.com>
2025-08-26 15:00:27 -0700

    Convert cypress test.

--- cypress/e2e/branch/molasses.cy.js
+++ cypress/e2e/branch/molasses.cy.js
@@ -5,13 +5,6 @@ import ModelOptions from "../../support/elements/ModelOptionPanel";
 const blocksTab = new BlocksTab();
 const modelOptions = new ModelOptions();
 
-// need to convert Blockly strings because they have &nbsp; in the DOM
-// but when run thru Cypress, Cypress converts it so `contain` never matches
-// function removeNBSP(text){
-//     const re = new RegExp(String.fromCharCode(160), "g");
-//     return text.replace(re, " ");
-// }
-
 context("Molasses Simulation", () => {
   beforeEach(() => {
     cy.visit("");
@@ -42,30 +35,16 @@ context("Molasses Simulation", () => {
       blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Compute lava flow impact").should("exist");
       blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Add data from flag").should("exist");
 
-    //   blocksTab.getTag('Data').click();
-    //   blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 13);
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(0).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("Name");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(1).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("4");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(2).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("Range from");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(6).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("Latitude, Longitude");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(8).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("+");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(9).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("Create a data table");
-    //   });
-    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).eq(10).text().then((text) => {
-    //       expect(removeNBSP(text)).to.containIgnoreCase("Create row in data table");
-    //   });
+      blocksTab.getTag('Data').click();
+      blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 13);
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Name").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("4").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Range from").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Latitude, Longitude").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("+").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create a data table").should("exist");
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create row in data table").should("exist");
     });
   });
 });
