7995968c8119368cfcc97466b8f48927d41160d6
Teale Fristoe <fristoe@gmail.com>
2025-08-26 15:25:23 -0700

    Cypress debugging.

--- cypress/e2e/branch/molasses.cy.js
+++ cypress/e2e/branch/molasses.cy.js
@@ -19,37 +19,37 @@ context("Molasses Simulation", () => {
   });
 
   describe('blocks', () => {
-    it('verify correct blocks are present', () => {
-      blocksTab.getTag('Volcano').should('be.visible');
-      blocksTab.getTag('Logic').should('be.visible');
-      blocksTab.getTag('Loops').should('be.visible');
-      blocksTab.getTag('Data').should('be.visible');
-      blocksTab.getTag('Variables').should('be.visible');
-      blocksTab.getTag('Functions').should('be.visible');
+    // it('verify correct blocks are present', () => {
+    //   blocksTab.getTag('Volcano').should('be.visible');
+    //   blocksTab.getTag('Logic').should('be.visible');
+    //   blocksTab.getTag('Loops').should('be.visible');
+    //   blocksTab.getTag('Data').should('be.visible');
+    //   blocksTab.getTag('Variables').should('be.visible');
+    //   blocksTab.getTag('Functions').should('be.visible');
 
-      blocksTab.getTag('Volcano').click();
-      blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 17);
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl())
-        .contains("Compute and visualize lava flow with...").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set eruption volume").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set lava front height").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set vent location").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set flag location").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Compute lava flow impact").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Add data from flag").should("exist");
+    //   blocksTab.getTag('Volcano').click();
+    //   blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 17);
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl())
+    //     .contains("Compute and visualize lava flow with...").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set eruption volume").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set lava front height").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set vent location").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set flag location").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Compute lava flow impact").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Add data from flag").should("exist");
 
-      blocksTab.getTag('Data').click();
-      blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 13);
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Name").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("4").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Range from").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Latitude, Longitude").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("+").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create a data table").should("exist");
-      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create row in data table").should("exist");
-    });
+    //   blocksTab.getTag('Data').click();
+    //   blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 13);
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Name").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("4").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Range from").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Latitude, Longitude").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("+").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create a data table").should("exist");
+    //   blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Create row in data table").should("exist");
+    // });
 
     it('running the simulation updates the table', () => {
       rightPanel.getDataTab().click();
