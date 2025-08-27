94f0e79f6d31438c6c3e88d26003ba038e676739
Teale Fristoe <fristoe@gmail.com>
2025-08-27 05:58:13 -0700

    Cypress debugging.

--- cypress/e2e/branch/molasses.cy.js
+++ cypress/e2e/branch/molasses.cy.js
@@ -27,9 +27,9 @@ context("Molasses Simulation", () => {
       blocksTab.getTag('Variables').should('be.visible');
       blocksTab.getTag('Functions').should('be.visible');
 
-      // blocksTab.getTag('Volcano').click();
-      // blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
-      // blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 17);
+      blocksTab.getTag('Volcano').click();
+      blocksTab.getFlyout().find(blocksTab.getBlockEl()).should('have.length', 7);
+      blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).should('have.length', 17);
       // blocksTab.getFlyout().find(blocksTab.getBlockTextEl())
       //   .contains("Compute and visualize lava flow with...").should("exist");
       // blocksTab.getFlyout().find(blocksTab.getBlockTextEl()).contains("Set eruption volume").should("exist");
