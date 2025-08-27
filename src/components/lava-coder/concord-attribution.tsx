036399429fe47e8420a90373f2741f030c14d3be
Kirk Swenson <kswenson@users.noreply.github.com>
2025-08-27 11:49:09 -0700

    chore: cesium license compliance (#425)

--- src/components/lava-coder/concord-attribution.tsx
+++ src/components/lava-coder/concord-attribution.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useRef } from "react";
 import ConcordLogo from "../../assets/concord.png";
 
 import "./concord-attribution.scss";
 
 export function ConcordAttribution() {
+  const attributionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // hide cesium "upgrade for commercial use" link
+    const parent = attributionRef.current?.closest(".lava-coder-view");
+    const cesiumUpgradeTextContainer = parent?.querySelector<HTMLDivElement>(".cesium-credit-textContainer");
+    if (cesiumUpgradeTextContainer) {
+      cesiumUpgradeTextContainer.style.display = "none";
+    }
+  });
+
   return (
     <>
       <div className="cesium-attribution-overlay" aria-hidden="true" />
-      <div className="concord-attribution">
+      <div className="concord-attribution" ref={attributionRef}>
         <img src={ConcordLogo} alt="Concord Consortium Logo" />
         <span>Concord Consortium</span>
       </div>
