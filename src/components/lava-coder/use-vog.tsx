158993e2aa4b4067cf6ddd9dbae13128aaa5a407
Teale Fristoe <fristoe@gmail.com>
2025-08-28 20:08:36 -0700

    Modify smoke based on distnace from vent.

--- src/components/lava-coder/use-vog.tsx
+++ src/components/lava-coder/use-vog.tsx
@@ -15,27 +15,34 @@ export function useVog(viewer: CesiumWidget | null, verticalExaggeration = 1) {
 
   useEffect(() => {
     return autorun(() => {
-      const { coveredCells, raster } = lavaSimulation;
+      const { coveredCells, raster, ventLatitude, ventLongitude } = lavaSimulation;
 
       if (!coveredCells || !lavaElevations || !gridBounds || !raster || !viewer || !cloudsRef.current) return;
 
       cloudsRef.current.removeAll();
 
-      Math.random();
+      const ventElevation = elevationAtPoint(ventLatitude, ventLongitude, raster);
       for (let i = 0; i < lavaElevations.length; i += 8) {
         for (let j = 0; j < lavaElevations[i].length; j += 8 + Math.floor(Math.random() * 8)) {
           const _i = i + Math.floor(Math.random() * 8);
           const lavaHeight = lavaElevations[_i] ? lavaElevations[_i][j] : undefined;
           if (lavaHeight && lavaHeight > 0) {
-            const long = gridBounds.west + (gridBounds.east - gridBounds.west) * (j / lavaElevations[_i].length);
             const lat = gridBounds.north - (gridBounds.north - gridBounds.south) * (_i / lavaElevations.length);
-            const elevation = elevationAtPoint(lat, long, raster) + (150 + Math.random() * 100) * verticalExaggeration;
-            console.log(` --`, lat, long, elevation);
+            const long = gridBounds.west + (gridBounds.east - gridBounds.west) * (j / lavaElevations[_i].length);
+            const dLat = lat - ventLatitude;
+            const dLong = long - ventLongitude;
+            const dist = Math.sqrt(dLat * dLat + dLong * dLong);
+            const _distFactor = Math.log(dist * 1000);
+            const distFactor = _distFactor ** 2;
+            const elevationFactor = Math.random() * 100 * verticalExaggeration;
+            const elevation = Math.max(elevationAtPoint(lat, long, raster) + elevationFactor,
+              ventElevation + (50 * distFactor * verticalExaggeration) + elevationFactor);
+            console.log(` --`, lat, long, dist, distFactor, elevation);
             cloudsRef.current.add({
               brightness: .75 + Math.random() * .15,
-              color: Color.YELLOW.withAlpha(.75),
+              color: Color.YELLOW.withAlpha(.8 - .01 * distFactor),
               position: Cartesian3.fromDegrees(long, lat, elevation),
-              scale: new Cartesian2(5000.0, 3000.0),
+              scale: new Cartesian2(3000 + 500 * distFactor, 2000 + 250 * distFactor),
               slice: .4 + Math.random() * .2,
               maximumSize: new Cartesian3(20.0, 8.0, 20.0)
             });
