bc9cbb7b644dd3a40ee08e8a4d04097e26e37435
Teale Fristoe <fristoe@gmail.com>
2025-11-06 14:20:34 -0800

    Position initial map.

--- src/hooks/lava-coder/use-show-wind-pattern.ts
+++ src/hooks/lava-coder/use-show-wind-pattern.ts
@@ -1,13 +1,21 @@
 import { Cartesian3, CesiumWidget, Entity, ImageMaterialProperty, Rectangle } from "@cesium/engine";
 import { reaction } from "mobx";
 import { useEffect, useRef } from "react";
-import { tradeWindsSvg } from "../../assets/lava-coder/wind-patterns/trade-winds-svg";
-import { tradeWindsDenseSvg } from "../../assets/lava-coder/wind-patterns/trade-winds-dense-svg";
+import tradeWindsPng from "../../assets/lava-coder/wind-patterns/trade-winds-arrows-w-island@3x.png";
+// import { tradeWindsSvg } from "../../assets/lava-coder/wind-patterns/trade-winds-svg";
+// import { tradeWindsDenseSvg } from "../../assets/lava-coder/wind-patterns/trade-winds-dense-svg";
 import { lavaSimulation } from "../../stores/lava-simulation-store";
-import { WindPattern } from "../../types/lava-coder/lava-coder-types";
-import { getWindData } from "../../utilities/vog-utilities";
+// import { WindPattern } from "../../types/lava-coder/lava-coder-types";
+// import { getWindData } from "../../utilities/vog-utilities";
 
 const heightMeters = 14000;
+
+const minLat = 18.38;
+const maxLat = 20.78;
+const midLat = (minLat + maxLat) / 2;
+const minLon = -156.67;
+const maxLon = -154.37;
+const midLon = (minLon + maxLon) / 2;
 
 export function useShowWindPattern(viewer: CesiumWidget | null) {
   const windPatternVisualizationRef = useRef<Entity | undefined>(undefined);
@@ -16,19 +24,19 @@ export function useShowWindPattern(viewer: CesiumWidget | null) {
       if (windPatternVisualizationRef.current) viewer?.entities.remove(windPatternVisualizationRef.current);
 
       if (show) {
-        const svg = lavaSimulation.windPattern === "trade" ? tradeWindsSvg : tradeWindsDenseSvg;
-        const svgUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+        // const svg = lavaSimulation.windPattern === "trade" ? tradeWindsSvg : tradeWindsDenseSvg;
+        // const svgUrl = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
 
-        const { minLat, maxLat, midLat, minLon, maxLon, midLon } =
-          // FIXME: Why is this case needed?
-          getWindData(lavaSimulation.windPattern as WindPattern);
+        // const { minLat, maxLat, midLat, minLon, maxLon, midLon } =
+        //   // FIXME: Why is this case needed?
+        //   getWindData(lavaSimulation.windPattern as WindPattern);
 
         windPatternVisualizationRef.current = viewer?.entities.add({
           position: Cartesian3.fromDegrees(midLon, midLat, heightMeters),
           rectangle: {
             coordinates: Rectangle.fromDegrees(minLon, minLat, maxLon, maxLat),
             material: new ImageMaterialProperty({
-              image: svgUrl,
+              image: tradeWindsPng,
               transparent: true
             }),
             height: heightMeters,
