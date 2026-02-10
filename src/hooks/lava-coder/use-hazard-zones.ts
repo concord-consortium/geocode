e73678e96d9810317f8fbf46532430817d983928
Teale Fristoe <fristoe@gmail.com>
2026-02-10 07:01:35 -0800

    Uncomment kml loading

--- src/hooks/lava-coder/use-hazard-zones.ts
+++ src/hooks/lava-coder/use-hazard-zones.ts
@@ -1,21 +1,21 @@
-import { CesiumWidget } from "@cesium/engine";
-// import { CesiumWidget, KmlDataSource } from "@cesium/engine";
+// import { CesiumWidget } from "@cesium/engine";
+import { CesiumWidget, KmlDataSource } from "@cesium/engine";
 import { useEffect } from "react";
-// import hazardZonesKml from "../../assets/Volcano_Lava_Flow_Hazard_Zones.kml";
+import hazardZonesKml from "../../assets/Volcano_Lava_Flow_Hazard_Zones.kml";
 import { lavaSimulation } from "../../stores/lava-simulation-store";
 
 export function useHazardZones(viewer: CesiumWidget | null, showHazardZones: boolean, verticalExaggeration: number) {
   const { hazardZones } = lavaSimulation;
 
-  // useEffect(() => {
-  //   // Load and overlay the KML file
-  //   KmlDataSource.load(hazardZonesKml, { clampToGround: true }).then((dataSource) => {
-  //     lavaSimulation.setHazardZones(dataSource);
-  //     dataSource.show = false; // Initially hide the KML data
-  //   }).catch((error) => {
-  //     console.error("Failed to load KML file:", error);
-  //   });
-  // }, []);
+  useEffect(() => {
+    // Load and overlay the KML file
+    KmlDataSource.load(hazardZonesKml, { clampToGround: true }).then((dataSource) => {
+      lavaSimulation.setHazardZones(dataSource);
+      dataSource.show = false; // Initially hide the KML data
+    }).catch((error) => {
+      console.error("Failed to load KML file:", error);
+    });
+  }, []);
 
   useEffect(() => {
     if (hazardZones) {
